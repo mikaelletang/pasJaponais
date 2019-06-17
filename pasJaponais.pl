bfaacd91a32bbf00b30406bd4e56eb9644ad6868
Fabien Tregan <treguy@free.Fr>
2019-06-17 17:59:32 +0200

    Update pasJaponais.pl
    Définie les règles des positions de départ et d'arrivée.

--- pasJaponais.pl
+++ pasJaponais.pl
@@ -1,20 +1,33 @@
 use_module(library(clpfd)).
 
-moyenne(A, B, C, Resultat) :-
-    Resultat is (A+B+C) / 3.
+red(Name) :-
+    test(Name,  Test),
+    L =.. Test,
+    not(call(L)).
 
-testMoyenne([A, B, C], SortieAttendue) :-
-    moyenne(A, B, C, SortieAttendue).
+even(X) :-
+    0 is mod(X, 2).
 
-test("La moyenne de 1, 2 et 3 est 2", testMoyenne([1, 2, 3], 2)).
-test("La moyenne de 2, 2 et 2 est 2", testMoyenne([2, 2, 2], 2)).
+fill([], _, 0).
 
-testPass(Test, Entree, SortieAttendue) :-
-	call(Test, Entree, SortieAttendue).
+fill([X|Xs], X, N) :-
+    succ(N0, N),
+    fill(Xs, X, N0).
 
-testPass([Test, Entree, SortieAttendue]) :-
-    test(Test),
-	testPass(Test, Entree, SortieAttendue).
-    
-testsPass(Tests) :-
-    maplist(testPass, Tests).
+depart(N,  Xs) :-
+    even(N),
+    N2 is N/2,
+    fill(Ds, regarde_a_droite, N2),
+    fill(Gs, regarde_a_gauche, N2),
+    append(Ds, [vide|Gs], Xs).
+
+arrivee(N, Xs) :-
+    depart(N, Ys),
+    reverse(Ys, Xs).
+
+
+test("position de départ à 2 joueurs", [depart, 2, [regarde_a_droite, vide, regarde_a_gauche]]).
+test("position de départ à 4 joueurs", [depart, 4, [regarde_a_droite, regarde_a_droite, vide, regarde_a_gauche, regarde_a_gauche]]).
+test("position d'arrivée à 2 joueurs", [depart, 2, [regarde_a_gauche, vide, regarde_a_droite]]).
+test("position d'arrivée à 4 joueurs", [depart, 4, [regarde_a_gauche, regarde_a_gauche, vide, regarde_a_droite, regarde_a_droite]]).
+
